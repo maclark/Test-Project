17769534182ee9e70a8bbbd1f52459abeb2780b0
maclark <max.a.clark@gmail.com>
2016-04-04 22:48:58 -0400

    Created ReplaySystem script with MyKeyFrame helper class.

---
+++ Assets/ReplaySystem.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame [bufferFrames];
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		Record ();
+	}
+
+	void PlayBack () {
+		rb.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		print ("reading frame " + frame);
+		transform.position = keyFrames [frame].position;
+		transform.rotation = keyFrames [frame].rotation;
+	}
+
+	void Record () {
+		rb.isKinematic = false;
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		print ("Writing frame " + frame);
+		keyFrames [frame] = new MyKeyFrame (time, transform.position, transform.rotation);
+	}
+}
+
+/// <summary>
+/// A structure for storing time, rotation and position.
+/// </summary>
+public class MyKeyFrame {
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame ( float time, Vector3 pos, Quaternion rot) {
+		frameTime = time;
+		position = pos;
+		rotation = rot;
+	}
+}
